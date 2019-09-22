cf7f16fd1879fb6add564b6b19a2d291fd0c26ea
fabianzielinski <fabz@o2.pl>
2019-09-22 15:03:22 +0200

    Change webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,41 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    watch: false,
-    mode: "development",
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [{
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                           modules: true
-                       }
+module.exports = (env) => {
+
+    const environment = env || 'production';
+
+    return {
+
+        mode: environment,
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.'+ environment +'.bundle.js'
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
                     }
-                ]
-            }
-        ]
-    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [{
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                               modules: true
+                           }
+                        }
+                    ]
+                }
+            ]
+        }
+    }    
 };
